d921a74931449c4ea23d34e880fa412408645347
dmytro <dmitro.geras@gmail.com>
2019-03-20 17:40:51 -0400

    Added controllers and models
    Added controllers: DriversController, TrucksController.
    Added Models: Driver, Truck

---
+++ FleetManagementSystem/FleetAPI/Models/Data/Driver.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace fleetAPI.Models.Data
+{
+    public partial class Driver
+    {
+        public Driver()
+        {
+            ShipmentOrder = new HashSet<ShipmentOrder>();
+        }
+
+        public int Id { get; set; }
+        public int? AddressId { get; set; }
+        public int CarrierId { get; set; }
+        public string LicenseType { get; set; }
+        public string LicenseNumber { get; set; }
+        public string LicenseState { get; set; }
+
+        public Address Address { get; set; }
+        public Carrier Carrier { get; set; }
+        [JsonIgnore]
+        public ICollection<ShipmentOrder> ShipmentOrder { get; set; }
+    }
+}
